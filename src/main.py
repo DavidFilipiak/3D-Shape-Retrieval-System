77bd922fba2e23b9247d3c70691f91c105160f14
DavidFilipiak <davidd.filipiak@gmail.com>
2023-09-19 13:57:52 +0200

    added inidcation of loaded meshes

--- src/main.py
+++ src/main.py
@@ -4,7 +4,10 @@ import os
 import pymeshlab
 import polyscope as ps
 from mesh import Mesh
+
+# GLOBAL VARIABLES
 ms = None
+listbox_loaded_meshes = None
 
 current_dir = os.getcwd()
 
@@ -12,21 +15,27 @@ def browse_button() -> None:
   db_dir = os.path.abspath(os.path.join(current_dir, "..", "db"))
   filename = filedialog.askopenfilename(title="Mesh select", initialdir=db_dir, filetypes=[('Mesh files', '*.obj')])
   ms.load_new_mesh(filename)
-  ms.show_polyscope()
+  listbox_loaded_meshes.insert(END, "/".join(filename.split("/")[-2:]))
+  #ms.show_polyscope()
 
 
 def main() -> None:
-  global ms
+  global ms, listbox_loaded_meshes
   ms = pymeshlab.MeshSet()
 
   root = Tk()
   root.title("3D Shape Retrieval")
   root.geometry("500x500")
 
-  label_browse = Label(root, text="Load Mesh into the app ")
-  label_browse.grid(row=0, column=1)
-  button_browse = Button(text="Load", command=browse_button)
-  button_browse.grid(row=0, column=3)
+  button_browse = Button(text="Load Mesh", command=browse_button)
+  button_browse.grid(row=0, column=1)
+  button_show = Button(text="Show Loaded Meshes", command=ms.show_polyscope)
+  button_show.grid(row=0, column=2)
+
+  label_loaded_meshes = Label(root, text="Loaded Meshes")
+  label_loaded_meshes.grid(row=1, column=1)
+  listbox_loaded_meshes = Listbox(root, width=50)
+  listbox_loaded_meshes.grid(row=2, column=1, columnspan=3)
 
   root.mainloop()
 
